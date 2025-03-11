68b4bfb88744877249b72ae828ce102ab6d3ce34
rapthegreat14 <descallarralph@gmail.com>
2025-03-11 20:48:41 +0800

    Add files via upload

---
+++ act lab 4.py
@@ -0,0 +1,18 @@
+score = int(input("Enter your score: "))
+
+if score == 100:
+    print("Grade: A")
+else:
+    if score == 90:
+        print("Grade: A")
+    else:
+        if score == 80:
+            print("Grade: B")
+        else:
+            if score == 70:
+                print("Grade: C")
+            else:
+                if score == 60:
+                    print("Grade: D")
+                else:
+                    print("Grade: F")
